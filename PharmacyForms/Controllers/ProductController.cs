5a22f3a8bbf49e410c60279f7622a7eb6d079712
Maksim <59286805+Maflend@users.noreply.github.com>
2021-09-16 18:05:59 +0300

    create productcontroller

---
+++ PharmacyForms/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using PhamacyLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyForms.Controllers
+{
+    public class ProductController
+    {
+        PharmacyContext db = new PharmacyContext();
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products.ToList();
+        }
+        public Product GetById(int id)
+        {
+            return db.Products.FirstOrDefault(i => i.Id == id);
+        }
+        public void Post (Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+        }
+    }
+}
